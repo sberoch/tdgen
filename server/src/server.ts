2de0343e59d113965c094356ed80677f08e2e4c2
Christoph Scholz <christoph.scholz@gmail.com>
2025-01-08 19:16:40 +0100

    remove MONGODB_PORT variable

--- server/src/server.ts
+++ server/src/server.ts
@@ -8,14 +8,13 @@ dotenv.config();
 
 const { MONGODB_ADMINUSERNAME = "admin" } = process.env;
 const { MONGODB_ADMINPASSWORD = "admin" } = process.env;
-const { MONGODB_HOST = "127.0.0.1" } = process.env;
+const { MONGODB_HOST = "127.0.0.1:27017" } = process.env;
 const { MONGODB_SCHEME = "mongodb" } = process.env;
-const { MONGODB_PORT = 27017 } = process.env;
 const { MONGODB_DB_NAME = "tdgen" } = process.env;
 const { CLIENT_DIST_DIR = "../client/dist/client/browser" } = process.env;
 const { APP_HTTP_PORT = 5200 } = process.env;
 
-const MONGODB_URI = `${MONGODB_SCHEME}://${MONGODB_ADMINUSERNAME}:${MONGODB_ADMINPASSWORD}@${MONGODB_HOST}:${MONGODB_PORT}`;
+const MONGODB_URI = `${MONGODB_SCHEME}://${MONGODB_ADMINUSERNAME}:${MONGODB_ADMINPASSWORD}@${MONGODB_HOST}`;
 
 connectToDatabase(MONGODB_URI, MONGODB_DB_NAME)
   .then(() => {
