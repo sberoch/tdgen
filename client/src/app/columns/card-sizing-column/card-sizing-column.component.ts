cde675625d02ced9a46a06f43c154a9f7d744dc0
Markus Nix <mnix@markusnix.com>
2025-02-03 20:06:17 -0300

    sizing column started

--- client/src/app/columns/card-sizing-column/card-sizing-column.component.ts
+++ client/src/app/columns/card-sizing-column/card-sizing-column.component.ts
@@ -1,7 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CardService } from '../../services/card.service';
+import { Card } from '../card-backlog-column/card-backlog-column.utils';
+import { getNextPastelColor } from '../card-backlog-column/card-backlog-column.utils';
+
+interface Section {
+  card: Card;
+  percentage: number;
+  color: string;
+}
 
 @Component({
   selector: 'app-card-sizing-column',
   templateUrl: './card-sizing-column.component.html',
 })
-export class CardSizingColumnComponent {}
+export class CardSizingColumnComponent implements OnInit {
+  cards: Card[] = [];
+  selectedCard: Card | null = null;
+  sections: Section[] = [];
+
+  constructor(private cardService: CardService) {}
+
+  ngOnInit() {
+    this.cardService.displayCards$.subscribe((cards) => {
+      this.cards = cards;
+      this.sections = cards.map((card, index) => ({
+        card,
+        percentage: 100 / cards.length,
+        color: getNextPastelColor(index),
+      }));
+    });
+
+    this.cardService.selectedCard$.subscribe((card) => {
+      this.selectedCard = card;
+    });
+  }
+
+  selectCard(card: Card) {
+    this.cardService.selectCard(card);
+  }
+}
