aeb95131c1f880a1d06329eb39518efb9aa98d69
Markus Nix <mnix@markusnix.com>
2025-03-17 16:31:06 -0300

    job tasks seed

---
+++ server/prisma/seed.ts
@@ -0,0 +1,73 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+const getRandomSentence = (minWords: number, maxWords: number) => {
+  const words = [
+    'Sonne',
+    'Mond',
+    'Sterne',
+    'Fluss',
+    'Berge',
+    'Tal',
+    'Wald',
+    'Haus',
+    'Tür',
+    'Fenster',
+    'Buch',
+    'Papier',
+    'Feder',
+    'Tinte',
+    'Baum',
+    'Straße',
+    'Auto',
+    'Reise',
+    'Zug',
+    'Flugzeug',
+    'Himmel',
+    'Meer',
+    'Welle',
+    'Musik',
+    'Lied',
+    'Tanz',
+    'Glück',
+    'Freude',
+    'Schmerz',
+    'Herz',
+    'Liebe',
+    'Freund',
+    'Garten',
+    'Blume',
+    'Dorf',
+    'Stadt',
+    'Licht',
+    'Schatten',
+    'Wind',
+    'Regen',
+  ];
+  const wordCount =
+    Math.floor(Math.random() * (maxWords - minWords + 1)) + minWords;
+  let sentence = '';
+  for (let i = 0; i < wordCount; i++) {
+    sentence += words[Math.floor(Math.random() * words.length)] + ' ';
+  }
+  return sentence.trim() + '.';
+};
+
+async function main() {
+  await prisma.jobTask.createMany({
+    data: Array.from({ length: 100 }, () => ({
+      title: getRandomSentence(3, 10).slice(0, 100),
+      text: getRandomSentence(50, 150).slice(0, 2000),
+      metadata: {},
+    })),
+  });
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+  })
+  .finally(() => {
+    void prisma.$disconnect();
+  });
