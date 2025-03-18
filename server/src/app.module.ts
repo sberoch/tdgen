6a609bd4c075b23be4667ab15342f0f77a7a09cf
Christoph Scholz <christoph.scholz@gmail.com>
2025-03-18 17:53:34 +0100

    make static path configurable by env

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,14 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
-import { join } from 'path';
+import { ConfigModule } from '@nestjs/config';
 import { EmployeesModule } from './employees/employees.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { JobTasksModule } from './job-tasks/job-tasks.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '../..', 'client'),
+      rootPath: process.env.CLIENT_DIST_DIR ?? '/usr/src/app/client',
     }),
     EmployeesModule,
     PrismaModule,
