9df531f0aab503b946496aec47756159d971576b
Markus Nix <mnix@markusnix.com>
2025-03-19 13:46:12 -0300

    removed employees

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { ConfigModule } from '@nestjs/config';
-import { EmployeesModule } from './employees/employees.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { JobTasksModule } from './job-tasks/job-tasks.module';
 import { JobDescriptionsModule } from './job-descriptions/job-descriptions.module';
@@ -13,7 +12,6 @@ import { AuthModule } from './auth/auth.module';
     ServeStaticModule.forRoot({
       rootPath: process.env.CLIENT_DIST_DIR ?? '/usr/src/app/client',
     }),
-    EmployeesModule,
     PrismaModule,
     JobTasksModule,
     JobDescriptionsModule,
