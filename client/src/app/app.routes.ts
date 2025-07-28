cf0d8618a4b656386d0957011fbe0ecc966c63e1
Markus Nix <mnix@markusnix.com>
2025-07-28 17:19:03 -0300

    denied route

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
+import { DeniedComponent } from './pages/denied/denied.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'denied', component: DeniedComponent },
 ];
