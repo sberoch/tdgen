d985367e09556314a3fb97b4f6249f16edf3c1f5
Markus Nix <mnix@markusnix.com>
2025-08-11 14:28:21 -0300

    small style fixes. config fixes

--- server/src/config/config.controller.ts
+++ server/src/config/config.controller.ts
@@ -6,8 +6,8 @@ export class ConfigController {
   getConfig() {
     return {
       apiUrl: process.env.API_URL ?? '',
-      adminRoleName: process.env.ADMIN_ROLE_NAME ?? 'admin',
-      userRoleName: process.env.USER_ROLE_NAME ?? 'user',
+      adminRoleName: process.env.TDGEN_ADMIN_ATTR_NAME ?? 'admin',
+      userRoleName: process.env.TDGEN_USER_ATTR_NAME ?? 'user',
       isDevEnv: process.env.NODE_ENV !== 'production',
     };
   }
